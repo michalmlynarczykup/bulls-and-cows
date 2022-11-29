33f68ebe13b3872c1abb99983551907e83020ca7
Michal Mlynarczyk <michal.mlynarczk2@student.up.krakow.pl>
2022-11-29 12:42:50 +0100

    refactor: change module

--- Data/Stats.py
+++ Data/Stats.py
@@ -5,3 +5,6 @@ class Stats:
     def __init__(self, bulls, cows):
         self.bulls = bulls
         self.cows = cows
+
+    def __repr__(self):
+        return f"Bulls = {self.bulls} & Cows = {self.cows}"
