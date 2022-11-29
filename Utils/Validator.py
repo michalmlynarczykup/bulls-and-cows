6844f4700f9efdea9ffca55bcbce6a1693fa27d0
Michal Mlynarczyk <michal.mlynarczk2@student.up.krakow.pl>
2022-11-29 12:43:18 +0100

    add: method for strings comparison

--- Utils/Validator.py
+++ Utils/Validator.py
@@ -1,15 +1,25 @@
+from Data.Stats import Stats
+
+
 class Validator:
-    host_word = None
+    def __init__(self):
+        pass
 
-    def __init__(self, host_word):
-        self.host_word = host_word
-
-    def validate(self, guesser_word):
-        if type(guesser_word) != str or not str.isalpha(guesser_word) or len(self.host_word) != len(guesser_word):
+    def validate(self, guesser_word, host_word):
+        if type(guesser_word) != str or not str.isalpha(guesser_word) or len(host_word) != len(guesser_word):
             return False
 
-        guesser_word = str.upper(guesser_word)
         if len(guesser_word) == len(set(guesser_word)):
             return True
         return False
 
+    def calculate_bulls_and_cows_in_attempt(self, guesser_word, host_word):
+        bulls = 0
+        cows = 0
+        for element in range(0, len(guesser_word)):
+            if guesser_word[element] == host_word[element]:
+                bulls += 1
+            elif guesser_word[element] in host_word:
+                cows += 1
+
+        return Stats(bulls, cows)
